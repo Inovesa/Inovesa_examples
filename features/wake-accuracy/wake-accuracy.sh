a2adeebc17c281556cee2b83f9082f3e5e3cd666
Patrik Schönfeldt <patrik@sedna.snft.de>
2019-09-30 08:48:09 +0200

    Rename wake accuracy plot script

--- features/wake-accuracy/wake-accuracy.sh
+++ features/wake-accuracy/wake-accuracy.sh
@@ -17,8 +17,7 @@ for padding in 0002 0004 0512 1024
 do
   basename="results/free-space-pad${padding}"
   ${inovesa_bin} -c wake-accuracy.cfg --padding ${padding} -o ${basename}.h5
-  ./wake-accuracy.py ${basename}.h5
+  ./plot_wake.py ${basename}.h5
   ${inovesa_bin} -c wake-accuracy.cfg --padding ${padding} -o ${basename}_png.h5 -i ${basename0}.png
-  ./wake-accuracy.py ${basename}_png.h5
+  ./plot_wake.py ${basename}_png.h5
 done
-
